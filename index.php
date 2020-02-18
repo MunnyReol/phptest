383586ff7f1dd746aa486c87809eb08a71eb78fe
okyk738 <60845352+okyk738@users.noreply.github.com>
2020-02-18 23:03:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 $channel = $_GET['c'];
-$data = file_get_contents("https://freewebtools.com/compteur-youtube/?channelId=$channel");
+$data = file_get_contents('https://freewebtools.com/compteur-youtube/?channelId=$channel');
 echo $data;
 ?>
