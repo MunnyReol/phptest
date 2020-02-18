83d242058216351600a5452541ee992d72bbff5b
okyk738 <60845352+okyk738@users.noreply.github.com>
2020-02-18 23:17:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 $channel = $_GET['c'];
 $data = file_get_contents('https://freewebtools.com/compteur-youtube/?channelId=UC-lHJZR3Gqxm24_Vd_AJ5Yw');
 $obj = json_decode($data);
-$subs = $obj->timestamp;
-
-echo $subs;
+$pubDate = $obj->publishedAt;
+$countAPI = $obj->subscriberCount;
+$timestamp = $obj->timestamp;
+$actualTimestamp = date() /1000
+echo $actualTimestamp;
 ?>
