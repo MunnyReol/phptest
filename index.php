63b19e4f6121b2ab29fe0fa43e89dda934aa4c5a
okyk738 <60845352+okyk738@users.noreply.github.com>
2020-02-18 23:10:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 $channel = $_GET['c'];
 $data = file_get_contents('https://freewebtools.com/compteur-youtube/?channelId=UC-lHJZR3Gqxm24_Vd_AJ5Yw');
-echo $data;
+$obj = json_decode($jsonobj);
+
+
+echo $obj->timestamp;
 ?>
