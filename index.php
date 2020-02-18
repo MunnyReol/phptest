410dd3795ad1298cb0db0189370894902573626a
okyk738 <60845352+okyk738@users.noreply.github.com>
2020-02-18 23:00:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-$result = file_get_contents('https://t1ch.com/api/GetUser.php?cid=UC-lHJZR3Gqxm24_Vd_AJ5Yw');
-echo $result;
+$channel = $_GET['c'];
+$result = file_get_contents('');
+echo $channel;
 ?>
